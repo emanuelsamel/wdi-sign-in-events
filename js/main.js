1b8a226e81ba9a550b68cfbc2f3d53ca1d8499af
emanuelsamel <mannys94@hotmail.com>
2017-07-20 15:10:06 -0400

    added error class

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', function(){
   var signIn = document.querySelector('.signin')
   var modal = document.querySelector('.modal')
   var button = document.querySelector('.close')
-
+  var submit = document.querySelector('.submit')
+  var error = document.getElementById('user')
+  var error2 = document.getElementById('pass')
 
 
 
@@ -15,5 +17,10 @@ document.addEventListener('DOMContentLoaded', function(){
   button.addEventListener('click', function(e){
     e.preventDefault();
       modal.style.display = 'none'
-  });
+  })
+  submit.addEventListener('click',function(e){
+    e.preventDefault();
+    error.classList.add("error");
+    error2.classList.add("error");
+  })
 })
