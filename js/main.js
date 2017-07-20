77b915b5120865dbbfa4925c2f9c44f0dc8054da
emanuelsamel <mannys94@hotmail.com>
2017-07-20 14:23:12 -0400

    step 2 model fade down

--- js/main.js
+++ js/main.js
@@ -2,10 +2,18 @@ document.addEventListener('DOMContentLoaded', function(){
 
   var signIn = document.querySelector('.signin')
   var modal = document.querySelector('.modal')
+  var button = document.querySelector('.close')
+
+
 
 
   signIn.addEventListener('click', function(e){
     e.preventDefault();
     modal.style.display = 'block'
   })
+
+  button.addEventListener('click', function(e){
+    e.preventDefault();
+      modal.style.display = 'none'
+  });
 })
